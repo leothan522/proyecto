8396be4fec7056ec5455c95cf4dc238eb4a9eda8
unknown <Administrador@ALGUARISA.LOCAL>
2021-03-03 17:29:42 -0400

    Prueba android buscar

--- resources/views/android/buscar_clap/index.blade.php
+++ resources/views/android/buscar_clap/index.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-    <nav class="navbar navbar-expand navbar-white navbar-light justify-content-center" style="border-radius: 10px">
+    <nav class="navbar navbar-expand navbar-navy fondo navbar-light justify-content-center" style="border-radius: 20px">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
             <li class="nav-item d-sm-inline-block">
-                <span class="nav-link active text-bold">Cedula</span>
+                <span class="nav-link active text-bold">Cédula</span>
             </li>
         </ul>
 
         <!-- SEARCH FORM -->
         <form class="form-inline ml-3">
             <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Ingrese numero" aria-label="Buscar" required>
+                <input class="form-control form-control-navbar bg-white" type="text" placeholder="Ingrese numero" aria-label="Buscar" required>
                 <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
+                    <button class="btn btn-navbar bg-white" type="submit">
                         <i class="fas fa-search"></i>
                     </button>
                 </div>
@@ -24,73 +24,5 @@
     </nav>
 
     <br>
-
-    <nav class="navbar navbar-expand navbar-teal navbar-light justify-content-center" style="border-radius: 10px">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item d-sm-inline-block">
-                <span class="nav-link">Cedula</span>
-            </li>
-        </ul>
-
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Ingrese numero" aria-label="Buscar" required>
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </nav>
-
-    <br>
-
-    <nav class="navbar navbar-expand navbar-info navbar-light justify-content-center" style="border-radius: 10px">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item d-sm-inline-block">
-                <span class="nav-link">Cedula</span>
-            </li>
-        </ul>
-
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Ingrese numero" aria-label="Buscar" required>
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </nav>
-
-    <br>
-
-    <nav class="navbar navbar-expand navbar-navy navbar-light justify-content-center" style="border-radius: 20px">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item d-sm-inline-block">
-                <span class="nav-link text-white">Cedula</span>
-            </li>
-        </ul>
-
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Ingrese numero" aria-label="Buscar" required>
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </nav>
-
 
 @endsection
